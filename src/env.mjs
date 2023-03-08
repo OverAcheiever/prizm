8df8f62c0cf30a476465826f984e5a624508f71e
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-03-08 18:56:40 +0000

    wip

--- src/env.mjs
+++ src/env.mjs
@@ -24,7 +24,7 @@ const client = z.object({
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const processEnv = {
-    DATABASE_URL: process.env.DATABASE_URL,
+    // DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
