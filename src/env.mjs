3623671449bd89a3da8b78b66fe3796965b71ba1
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-03-14 19:47:17 +0530

    wip

--- src/env.mjs
+++ src/env.mjs
@@ -5,9 +5,9 @@ import { z } from "zod";
  * built with invalid env vars.
  */
 const server = z.object({
-  DATABASE_URL: z.string().url(),
-  TREASURY: z.string(),
-  NODE_ENV: z.enum(["development", "test", "production"]),
+    DATABASE_URL: z.string().url(),
+    TREASURY: z.string(),
+    NODE_ENV: z.enum(["development", "test", "production"]),
 });
 
 /**
@@ -15,7 +15,8 @@ const server = z.object({
  * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 const client = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+    NEXT_MAGIC_KEY: z.string(),
+    // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
 });
 
 /**
@@ -25,10 +26,11 @@ const client = z.object({
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const processEnv = {
-  DATABASE_URL: process.env.DATABASE_URL,
-  TREASURY: process.env.TREASURY,
-  NODE_ENV: process.env.NODE_ENV,
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    DATABASE_URL: process.env.DATABASE_URL,
+    TREASURY: process.env.TREASURY,
+    NEXT_MAGIC_KEY: process.env.NEXT_MAGIC_KEY,
+    NODE_ENV: process.env.NODE_ENV,
+    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
 // Don't touch the part below
@@ -43,36 +45,37 @@ const merged = server.merge(client);
 let env = /** @type {MergedOutput} */ (process.env);
 
 if (!!process.env.SKIP_ENV_VALIDATION == false) {
-  const isServer = typeof window === "undefined";
+    const isServer = typeof window === "undefined";
 
-  const parsed = /** @type {MergedSafeParseReturn} */ (
-    isServer
-      ? merged.safeParse(processEnv) // on server we can validate all env vars
-      : client.safeParse(processEnv) // on client we can only validate the ones that are exposed
-  );
-
-  if (parsed.success === false) {
-    console.error(
-      "❌ Invalid environment variables:",
-      parsed.error.flatten().fieldErrors
+    const parsed = /** @type {MergedSafeParseReturn} */ (
+        isServer ?
+        merged.safeParse(processEnv) // on server we can validate all env vars
+        :
+        client.safeParse(processEnv) // on client we can only validate the ones that are exposed
     );
-    throw new Error("Invalid environment variables");
-  }
 
-  env = new Proxy(parsed.data, {
-    get(target, prop) {
-      if (typeof prop !== "string") return undefined;
-      // Throw a descriptive error if a server-side env var is accessed on the client
-      // Otherwise it would just be returning `undefined` and be annoying to debug
-      if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
-        throw new Error(
-          process.env.NODE_ENV === "production"
-            ? "❌ Attempted to access a server-side environment variable on the client"
-            : `❌ Attempted to access server-side environment variable '${prop}' on the client`
+    if (parsed.success === false) {
+        console.error(
+            "❌ Invalid environment variables:",
+            parsed.error.flatten().fieldErrors
         );
-      return target[/** @type {keyof typeof target} */ (prop)];
-    },
-  });
+        throw new Error("Invalid environment variables");
+    }
+
+    env = new Proxy(parsed.data, {
+        get(target, prop) {
+            if (typeof prop !== "string") return undefined;
+            // Throw a descriptive error if a server-side env var is accessed on the client
+            // Otherwise it would just be returning `undefined` and be annoying to debug
+            if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
+                throw new Error(
+                    process.env.NODE_ENV === "production" ?
+                    "❌ Attempted to access a server-side environment variable on the client" :
+                    `❌ Attempted to access server-side environment variable '${prop}' on the client`
+                );
+            return target[ /** @type {keyof typeof target} */ (prop)];
+        },
+    });
 }
 
 export { env };
